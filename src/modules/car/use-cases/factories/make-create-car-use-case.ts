725b29c912db4091d74b33f413089ef4f77feaf4
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-11-14 21:26:25 -0300

    alter name plural

--- src/modules/car/use-cases/factories/make-create-car-use-case.ts
+++ src/modules/car/use-cases/factories/make-create-car-use-case.ts
@@ -1,8 +1,8 @@
-import { PostgresCarRepository } from '@car/repositories/postgres/postgres-car-repository'
+import { PostgresCarsRepository } from '@car/repositories/postgres/postgres-car-repository'
 import { CreateCarUseCase } from '../create-car-use-case'
 
 export function makeCreateCarUseCase (): CreateCarUseCase {
-  const carRepository = new PostgresCarRepository()
+  const carRepository = new PostgresCarsRepository()
   const createCarUseCase = new CreateCarUseCase(carRepository)
 
   return createCarUseCase

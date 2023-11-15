5b0b4f3e2bb501458e537b606db7e5281a2c9579
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-11-14 21:19:32 -0300

    create category test

---
+++ src/modules/car/repositories/in-memory/in-memory-categories-repository.ts
@@ -0,0 +1,33 @@
+import { ICategoriesRepository } from '@car/repositories/ICategories-repository'
+import { Category, Prisma } from '@prisma/client'
+import { randomUUID } from 'crypto'
+
+ export class InMemoryCategoriesRepository implements ICategoriesRepository {
+  categories: Category[] = []
+
+  async create({name,  description}: Prisma.CategoryCreateInput) :Promise<void> {
+    const category: Category ={
+      id: randomUUID(),
+      name,
+      description,
+      created_at: new Date()
+    }
+
+    this.categories.push(category)
+  }
+
+  async list(): Promise<Category[]> {
+    return this.categories
+  }
+
+  async findByName(name: string): Promise<Category | null> {
+    const category = this.categories.find(item => item.name === name)
+
+    if(!category){
+      return null
+    }
+
+    return category
+  }
+
+}
---
+++ src/modules/car/use-cases/create-category-use-case.spec.ts
@@ -0,0 +1,57 @@
+import { CreateCategoryDTO } from '@car/DTOs/create-category-dto'
+import { InMemoryCategoriesRepository } from "@car/repositories/in-memory/in-memory-categories-repository"
+import { CreateCategoryUseCase } from '@car/use-cases/create-category-use-case'
+import { beforeEach, describe, expect, it } from 'vitest'
+import { CategoryAlreadyExistError } from './errors/category-already-exist-error'
+
+let categoriesRepository: InMemoryCategoriesRepository
+let createCategoryUseCase: CreateCategoryUseCase
+
+describe('Create category use case', () => {
+
+  beforeEach(() => {
+    categoriesRepository = new InMemoryCategoriesRepository()
+    createCategoryUseCase = new CreateCategoryUseCase(categoriesRepository)
+  })
+
+  it('Should be able to create a new category car', async () => {
+
+    const category: CreateCategoryDTO = {
+      name: 'SUV',
+      description: `
+        A sigla SUV significa Sport Utility Vehicle,
+        Os carros dessa categoria costumam ser reconhecidos por 
+        apresentarem porte robusto, interior espaçoso e por transitar com excelente desempenho na cidade ou no off-road.
+        Além do espaço interior e porte avantajado, o que não pode faltar em um automóvel SUV é a tração nas quatro rodas, 
+        o 4X4 que conhecemos. Os motoristas realmente são conquistados pelo alto desempenho dos veículos e pela segurança.
+      `
+    }
+
+    await createCategoryUseCase.execute(category)
+
+    const categoryCreated = await categoriesRepository.findByName(category.name)
+
+    expect(categoryCreated).not.toEqual(null)
+    expect(categoryCreated).toHaveProperty('id')
+
+  })
+
+  it('Should not be able to create two category with same name', async () => {
+    const category: CreateCategoryDTO = {
+      name: 'SUV',
+      description: `
+        A sigla SUV significa Sport Utility Vehicle,
+        Os carros dessa categoria costumam ser reconhecidos por 
+        apresentarem porte robusto, interior espaçoso e por transitar com excelente desempenho na cidade ou no off-road.
+        Além do espaço interior e porte avantajado, o que não pode faltar em um automóvel SUV é a tração nas quatro rodas, 
+        o 4X4 que conhecemos. Os motoristas realmente são conquistados pelo alto desempenho dos veículos e pela segurança.
+      `
+    }
+
+    await createCategoryUseCase.execute(category)
+
+    await expect(() => {
+      return createCategoryUseCase.execute({name: 'SUV', description: 'A sigla SUV significa Sport Utility Vehicle'})
+    }).rejects.toBeInstanceOf(CategoryAlreadyExistError)
+  })
+})

7dbf68e68bfd64e445edc82af91466207102169f
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-11-16 18:27:45 -0300

    change cars repository name

--- src/modules/car/use-cases/factories/make-create-specification-use-case.ts
+++ src/modules/car/use-cases/factories/make-create-specification-use-case.ts
@@ -1,9 +1,9 @@
-import { PostgresCarRepository } from '@car/repositories/postgres/postgres-car-repository'
+import { PostgresCarsRepository } from '@car/repositories/postgres/postgres-car-repository'
 import { PostgresSpecificationsRepository } from '@car/repositories/postgres/postgres-specification-repository'
 import { CreateSpecificationUseCase } from '../create-specification-use-case'
 
 export function makeCreateSpecificationUseCase (): CreateSpecificationUseCase {
-  const carsRepository = new PostgresCarRepository()
+  const carsRepository = new PostgresCarsRepository()
   const specificationRepository = new PostgresSpecificationsRepository()
   const createCarSpecificationUseCase = new CreateSpecificationUseCase(
     specificationRepository,

473d4f9670f82438076f639a8f54faf9fe195d90
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-11-16 18:26:50 -0300

    instace categories repository

--- src/modules/car/use-cases/factories/make-create-car-use-case.ts
+++ src/modules/car/use-cases/factories/make-create-car-use-case.ts
@@ -1,9 +1,11 @@
 import { PostgresCarsRepository } from '@car/repositories/postgres/postgres-car-repository'
+import { PostgresCategoriesRepository } from '@car/repositories/postgres/postgres-categories-repository'
 import { CreateCarUseCase } from '../create-car-use-case'
 
 export function makeCreateCarUseCase (): CreateCarUseCase {
   const carRepository = new PostgresCarsRepository()
-  const createCarUseCase = new CreateCarUseCase(carRepository)
+  const categoriesRepository = new PostgresCategoriesRepository()
+  const createCarUseCase = new CreateCarUseCase(carRepository, categoriesRepository)
 
   return createCarUseCase
 }

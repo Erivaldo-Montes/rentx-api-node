acfbf0cea342388ab9b8969f9bed75abcd0acb2d
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-11-18 15:39:26 -0300

    update car controller

---
+++ src/modules/car/http/update-car-controller.ts
@@ -0,0 +1,33 @@
+import { makeUpdateUseCase } from '@car/use-cases/factories/make-update-car-use-case';
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from 'zod';
+
+export async function UpdateCarController(request: FastifyRequest, reply: FastifyReply): Promise<FastifyReply> {
+  const updateCarSchemaBody = z.object({
+    name: z.string(),
+    brand: z.string(),
+    about: z.string(),
+    category_id: z.string(),
+    daily_rate: z.number()
+  })
+
+  const updateCarSchemaParams = z.object({
+    id: z.string()
+  })
+
+  const {name, brand, about, category_id, daily_rate} = updateCarSchemaBody.parse(request.body)
+  const {id} = updateCarSchemaParams.parse(request.params)
+
+  const updateCarUseCase = makeUpdateUseCase()
+
+  const car = await updateCarUseCase.execute({
+    id,
+    name,
+    brand,
+    about,
+    category_id,
+    daily_rate
+  })
+
+  return reply.status(200).send({car})
+}

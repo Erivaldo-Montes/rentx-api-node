82aa5efdb80e5503840a283a7e4c1cb323fe8c1d
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-11-18 15:42:35 -0300

    update car use case factory

---
+++ src/modules/car/use-cases/factories/make-update-car-use-case.ts
@@ -0,0 +1,9 @@
+import { PostgresCarsRepository } from "@car/repositories/postgres/postgres-car-repository";
+import { UpdateCarUseCase } from "../update-car-use-case";
+
+export function makeUpdateUseCase(): UpdateCarUseCase {
+  const carRepository = new PostgresCarsRepository()
+  const updateCarUseCase = new UpdateCarUseCase(carRepository)
+
+  return updateCarUseCase
+}

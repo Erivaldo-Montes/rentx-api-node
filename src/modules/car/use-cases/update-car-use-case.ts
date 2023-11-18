0c03cbec1bf360ee033e70808f9861cfe744aef3
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-11-18 15:42:11 -0300

    update car use case

---
+++ src/modules/car/use-cases/update-car-use-case.ts
@@ -0,0 +1,20 @@
+import { UpdateCarDTO } from '@car/DTOs/update-car-dto';
+import { Car } from '@prisma/client';
+import { ICarsRepository } from "../repositories/ICars-repository";
+import { CarNotExistError } from './errors/car-not-exist-error';
+
+export class UpdateCarUseCase {
+  constructor(private carsRepository: ICarsRepository){}
+
+  async execute({id ,name, brand, about, category_id, daily_rate}:UpdateCarDTO ): Promise<Car>{
+    const car = await this.carsRepository.findById(id)
+
+    if(!car){
+      throw new CarNotExistError
+    }
+
+    const carUpdate = await this.carsRepository.update({id, name, about, brand, category_id, daily_rate})
+    
+    return carUpdate
+  }
+}

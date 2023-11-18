1cc10bb9b79d7094a7c79ad2f9bb1e95f0bb55e9
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-11-18 15:43:08 -0300

    update car route

--- src/modules/car/http/routes.ts
+++ src/modules/car/http/routes.ts
@@ -5,11 +5,13 @@ import { createCarSpecificationController } from './create-specification-control
 import { deleteCarController } from './delete-car-controller'
 import { deleteCategoryController } from './delete-category-controller'
 import { listCategoriesController } from './list-category-controller'
+import { UpdateCarController } from './update-car-controller'
 
 export async function carRoute (app: FastifyInstance): Promise<void> {
   app.post('/specification/:carId', createCarSpecificationController)
   app.post('/car', createCarController)
   app.delete('/car/:carId',deleteCarController )
+  app.patch('/car/:id', UpdateCarController)
   app.post('/category', createCategoryController)
   app.get('/category', listCategoriesController)
   app.delete('/category/:id', deleteCategoryController)

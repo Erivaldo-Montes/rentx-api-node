f6a2c18c86339a334f0ddd1dd205c17d87f947dc
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-11-19 15:31:51 -0300

    list car controller

---
+++ src/modules/car/http/list-car-controller.ts
@@ -0,0 +1,17 @@
+import { makeListCarsUseCase } from '@car/use-cases/factories/make-list-cars-use-case';
+import { FastifyReply, FastifyRequest } from "fastify";
+import { z } from 'zod';
+
+export async function listCarsController (request: FastifyRequest, reply: FastifyReply): Promise<FastifyReply>{
+  const listCarsSchemaQuery =  z.object({
+    page: z.coerce.number().min(1).default(1)
+  }) 
+
+  const { page } = listCarsSchemaQuery.parse(request.query)
+
+  const listCarsUseCase = makeListCarsUseCase()
+
+  const cars = await listCarsUseCase.execute({page})
+
+  return reply.status(200).send(cars)
+}

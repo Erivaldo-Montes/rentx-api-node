32672c56c6c617497d1d57b4ae0049365d44f4fd
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-11-19 15:37:52 -0300

    list car factory

---
+++ src/modules/car/use-cases/factories/make-list-cars-use-case.ts
@@ -0,0 +1,9 @@
+import { PostgresCarsRepository } from "../../repositories/postgres/postgres-car-repository";
+import { ListCarsUseCase } from "../list-cars-use-case";
+
+export function makeListCarsUseCase(): ListCarsUseCase {
+  const carsRepository = new PostgresCarsRepository()
+  const listCarsUseCase = new ListCarsUseCase(carsRepository)
+
+  return listCarsUseCase
+}

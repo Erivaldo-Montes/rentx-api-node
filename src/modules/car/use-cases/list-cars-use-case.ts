529b2f9966c8af5831e7b714523ae4d42a60aba0
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-11-19 15:37:30 -0300

    list car use case

---
+++ src/modules/car/use-cases/list-cars-use-case.ts
@@ -0,0 +1,11 @@
+import { ListCarsDTO } from '@car/DTOs/list-cars-dto'
+import { Car } from '@prisma/client'
+import { ICarsRepository } from "../repositories/ICars-repository"
+
+export class ListCarsUseCase {
+  constructor(private carsRepository: ICarsRepository){}
+
+  async execute({page}: ListCarsDTO): Promise<Car[]> {
+    return await this.carsRepository.list({page})
+  }
+}

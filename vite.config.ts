26e83193a3511241ca182431e72ab94f3a259f44
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-11-23 15:56:21 -0300

    environment match globs

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import { defineConfig } from 'vitest/config'
 export default defineConfig({
   plugins: [tsconfigPaths()],
   test: {
-    environmentMatchGlobs: [['src/http/**', 'prisma']]
+    environmentMatchGlobs: [['src/modules/**/use-cases', 'prisma']]
   }
 })

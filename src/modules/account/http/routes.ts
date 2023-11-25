9909b3b484814e99a9b6b2aa6edafa8f1a7ddaaa
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-11-25 10:14:47 -0300

    authenticate user route

--- src/modules/account/http/routes.ts
+++ src/modules/account/http/routes.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
+import { authenticateUserController } from './authenticate-user-controller';
 import { createUserController } from './create-user-controller';
 
 export async function userRoute(app: FastifyInstance): Promise<void> {
+  app.post('/session', authenticateUserController)
   app.post('/user', createUserController)
 }

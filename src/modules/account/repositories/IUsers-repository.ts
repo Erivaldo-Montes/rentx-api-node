efec2f4a05060766f29c4b7fc5064cd959bfcd59
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-11-25 10:15:27 -0300

    return user

--- src/modules/account/repositories/IUsers-repository.ts
+++ src/modules/account/repositories/IUsers-repository.ts
@@ -2,6 +2,6 @@ import { Prisma, User } from "@prisma/client"
 
 export interface IUsersRepository {
   create({name, email, password, driver_license  }: Prisma.UserCreateInput): Promise<Omit<User, 'password' | 'role' >>
-  findByEmail(name: string): Promise<Omit<User, 'password'| 'role'> | null>
+  findByEmail(name: string): Promise<User | null>
   findByDriverLicense(driver_license: string): Promise<Omit<User, 'password' | 'role'> | null>
 }

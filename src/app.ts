449b3be31ef80f9ef55a2ea1481254c26bb0b745
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-11-25 10:13:31 -0300

    jwt config

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,21 @@
 import { userRoute } from '@account/http/routes'
 import { carRoute } from '@car/http/routes'
+import fastifyJwt from '@fastify/jwt'
 import swagger from '@fastify/swagger'
 import swaggerUI from '@fastify/swagger-ui'
 import Fastify from 'fastify'
 import { ZodError } from 'zod'
+import { jwtConfig } from './config/auth'
 
 export const app = Fastify({
   logger: true
 })
 
+app.register(fastifyJwt, {
+  secret: jwtConfig.secret
+})
 
-
+// swagger
 app.register(swagger, {
   mode: 'static',
   specification: {
@@ -28,9 +33,11 @@ app.register(swaggerUI, {
 })
 
 // eslint-disable-next-line @typescript-eslint/no-floating-promises
+// routes
 app.register(carRoute)
 app.register(userRoute)
 
+// error handler
 app.setErrorHandler((error, request, reply) => {
   if (error instanceof ZodError) {
     return reply

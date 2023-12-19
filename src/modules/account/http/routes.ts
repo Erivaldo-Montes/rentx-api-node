d7d878d8ac0b5d0259b1340f41821976639ddfce
Erivaldo-Montes <erivaldomontez@gmail.com>
2023-12-18 21:15:56 -0300

    update password route

--- src/modules/account/http/routes.ts
+++ src/modules/account/http/routes.ts
@@ -4,10 +4,16 @@ import { authenticateUserController } from './authenticate-user-controller'
 import { createUserController } from './create-user-controller'
 import { GetUserProfileController } from './get-user-profile-controller'
 import { refreshTokenController } from './refresh-token-controller'
+import { updatePasswordController } from './update-password-controller'
 
 export async function userRoute(app: FastifyInstance): Promise<void> {
   app.post('/user', createUserController)
   app.post('/session', authenticateUserController)
   app.get('/me', { onRequest: [ensureAuthenticate] }, GetUserProfileController)
   app.post('/refresh-token', refreshTokenController)
+  app.post(
+    '/user/update-password',
+    { onRequest: [ensureAuthenticate] },
+    updatePasswordController,
+  )
 }
